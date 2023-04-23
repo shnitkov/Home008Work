d05199aa0775a114cee7d8b81ff62a1c128ec777
Shnitkov <shnitkov2014@mail.ru>
2023-04-24 00:12:45 +0300

    Задача 21

---
+++ homework/Homework21.cs
@@ -0,0 +1,31 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.  A (3,6,8); B (2,1,-7), -> 15.84  A (7,-5, 0); B (1,-1,9) -> 11.53
+
+int InputN(string message)
+{
+System.Console.Write($"{message}> ");
+int value;
+if (int.TryParse(Console.ReadLine(), out value))
+{
+return value;
+}
+System.Console.WriteLine("Вы ввели не число");
+Environment.Exit(1);
+return 0;
+
+}
+
+int x1 = InputN ("Введите координату X точки A");
+int y1 = InputN ("Введите координату Y точки A");
+int z1 = InputN ("Введите координату Z точки A");
+int x2 = InputN ("Введите координату X точки B");
+int y2 = InputN ("Введите координату Y точки B");
+int z2 = InputN ("Введите координату Z точки B");
+
+double dis(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+int deltax = x2 - x1;
+int deltay = y2 - y1;
+int deltaz = z2 - z1;
+return Math.Sqrt(deltax * deltax + deltay * deltay + deltaz * deltaz);
+}
+System.Console.Write($"Растояние между точками A и B равно {dis(x1, y1, z1, x2, y2, z2):F2} ");
